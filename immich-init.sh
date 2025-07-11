47265c1d486e394cf558b0405ca3153caff9eb36
Zak <34372536+ZakShearman@users.noreply.github.com>
2025-07-11 13:03:46 +0100

    chore: remove old files

--- immich-init.sh
+++
@@ -1,3 +0,0 @@
-helm install immich immich/immich --namespace immich-system --version 0.8.3 -f immich-values.yaml --wait
-
-# helm upgrade immich immich/immich --namespace immich-system --version 0.8.3 -f immich-values.yaml --wait
